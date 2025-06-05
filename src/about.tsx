a2bacba7cda8c172d5ddb43dd496e749b7159bd7
Ivy Wills <ivy.wills@mail.utoronto.ca>
2025-06-05 10:17:53 -0400

    fix: add the seal

--- src/about.tsx
+++ src/about.tsx
@@ -1,3 +1,13 @@
+import Seal from './assets/seal.png';
+
 export default function About() {
-  return <h1>About Page</h1>;
+  return (
+    <>
+      {/* ...navbar is rendered by parent layout... */}
+      <h1 className="text-2xl font-bold text-center m-4">About Us</h1>
+      <div className="flex flex-col items-center justify-center my-4 max-h-[33vh] max-w-screen">
+        <img src={Seal} className="max-h-[33vh] w-auto object-contain" />
+      </div>
+    </>
+  );
 }
