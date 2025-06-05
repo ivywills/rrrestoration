dd4d8e07affe401b8ec95cfac750158b7a6ca823
Ivy Wills <ivy.wills@mail.utoronto.ca>
2025-06-05 10:48:02 -0400

    fix: add hover card to about page

--- src/about.tsx
+++ src/about.tsx
@@ -23,19 +23,19 @@ export default function About() {
       <div className="text-center text-lg font-semibold text-secondary m-2 min-h-[2rem]">
         {message.slice(0, visibleLetters)}
       </div>
-      <div className="card border-2 border-accent-content bg-base-100 w-full shadow-sm transition-transform duration-200 hover:scale-101 hover:shadow-lg">
-        <div className="text-md font-semibold mt-4 mb-2 mx-4">
+      <div className="card border-2 border-accent-content bg-neutral-content w-11/12 max-w-xl mx-auto shadow-sm transition-transform duration-200 hover:scale-105 hover:shadow-lg my-2">
+        <div className="text-md font-semibold mt-4 mb-1 mx-8">
           Together, for the past 10 years, we have had the opportunity to serve
           numerous clients in Windsor-Essex County. The majority of our work is
           focused on restoring your home or business to its original condition.
         </div>
-        <div className="text-md font-semibold m-2 mx-4">
+        <div className="text-md font-semibold m-1 mx-8">
           Your property is one of the biggest investments you'll make in your
           life, and far too often, we see these investments depreciate in value.
           At RR, we take pride in helping our clients restore and protect their
           investments.
         </div>
-        <div className="text-md font-semibold mt-2 mb-4 mx-4">
+        <div className="text-md font-semibold mt-1 mb-4 mx-8">
           If you're interested in what we can do for your property, please
           contact us for a free estimate.
         </div>
