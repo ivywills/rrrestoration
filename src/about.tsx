bbf338db01d3cf06164bdfcb9b267c67e1b2ae91
Ivy Wills <ivy.wills@mail.utoronto.ca>
2025-06-05 10:18:30 -0400

    fix: remove comment

--- src/about.tsx
+++ src/about.tsx
@@ -3,7 +3,6 @@ import Seal from './assets/seal.png';
 export default function About() {
   return (
     <>
-      {/* ...navbar is rendered by parent layout... */}
       <h1 className="text-2xl font-bold text-center m-4">About Us</h1>
       <div className="flex flex-col items-center justify-center my-4 max-h-[33vh] max-w-screen">
         <img src={Seal} className="max-h-[33vh] w-auto object-contain" />
